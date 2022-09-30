51f327b3a409304b8717bd0d288bf09d2334d2b7
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 18:07:45 +0200

    ex8

--- ex-08.js
+++ ex-08.js
@@ -1,1 +1,9 @@
 "use strict"
+const image = document.querySelector('img')
+
+function changeImage() {
+    this.src = this.src == 'https://picsum.photos/id/1025/600/400' ? 'https://picsum.photos/id/1020/600/400' : 'https://picsum.photos/id/1025/600/400'
+    this.alt = this.alt == 'Un gentil chien' ? 'Un gentil ours' : 'Un gentil chien'
+}
+
+image.addEventListener('click', changeImage)
