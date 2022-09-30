3edd74cf926e42ba4378c152ffaa3028025e50ff
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 16:59:41 +0200

    ex6

--- ex-06.js
+++ ex-06.js
@@ -1,1 +1,13 @@
 "use strict"
+const grid = document.getElementById('app')
+
+grid.style.display = 'grid'
+grid.style.gridTemplateColumns = 'repeat(3, 6rem)'
+
+for(let i=0; i < 9; i++) {
+    const gridItem = document.createElement('div')
+    gridItem.style.width = '6rem'
+    gridItem.style.height = '6rem'
+    gridItem.style.border = '1px solid #ccc'
+    grid.append(gridItem)
+}
