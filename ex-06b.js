3f2522cba07b8f4a48005af7fd7589b1bea10e18
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 15:52:20 +0200

    first commit

---
+++ ex-06b.js
@@ -0,0 +1,21 @@
+let grid = {
+    attribute: {
+        id: 'morpion' 
+    },
+    style: {
+        width: '27rem',
+        height: '27rem',
+        display: 'grid',
+        'grid-template-columns': 'repeat(3, 9rem)',
+        'grid-template-rows': 'repeat(3, 9rem)',
+    }
+}
+
+let box = {
+    style: {
+        border: '1px solid #ccc',
+        display: 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+    }
+}
