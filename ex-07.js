741a5e79c184e5c5a6ebb806a90b972285190430
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 17:57:04 +0200

    ex7

--- ex-07.js
+++ ex-07.js
@@ -1,1 +1,8 @@
 "use strict"
+for(let element of document.querySelectorAll('li')) {
+    element.addEventListener('click', () => {
+        document.querySelector('body').style[element.dataset.toggle] = element.dataset.value
+    })
+}
+
+
