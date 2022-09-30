64f994a17f7309594a9d4329608c40f33f36cbf2
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 16:23:33 +0200

    ex4

--- ex-04.js
+++ ex-04.js
@@ -1,1 +1,6 @@
 "use strict"
+const parent = document.querySelector('nav>ul>li>a')
+const menu = document.querySelector('nav>ul>li>ul')
+parent.addEventListener('click', () => {
+    menu.style.display = menu.style.display == '' ? 'none' : ''
+})
