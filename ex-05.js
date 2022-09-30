f1086bdf608b08e6190eb6b6e83425185598b71c
Virginie Morque <v.morque@capitechnic.com>
2022-09-30 16:46:45 +0200

    ex5

--- ex-05.js
+++ ex-05.js
@@ -1,1 +1,16 @@
 "use strict"
+const parentList = document.querySelectorAll('[data-toggle=dropdown]')
+
+function hideMenus(parentShow = null) {
+    for(parent of parentList) {
+        const menu = parent.nextElementSibling
+        menu.style.display = parent == parentShow ? '' : 'none'
+    }
+}
+
+hideMenus()
+for(parent of parentList) {
+    parent.addEventListener('click', (e) => {
+        hideMenus(e.target)
+    })
+}
